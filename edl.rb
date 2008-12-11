c9f60fa81d0dbd5bed3e7580c611ca70d78ddc4e
julik <julik@65967661-7906-0410-b21e-e24373c15fab>
2008-12-11 03:04:03 +0000

    We are getting somewewhere on this EDL front. At least now we know how transitions workl

--- edl.rb
+++ edl.rb
@@ -1,6 +1,97 @@
 require File.dirname(__FILE__) + "/../timecode/timecode"
 
+# A simplistic EDL parser. Current limitations: no support for DF timecode, no support for audio
 module EDL
+  
+  # Represents an EDL, is returned from the parser. Traditional operation is functional style, i.e.
+  #
+  #  edl.renumbeder.without_dissolves.without_generators.as_edit_of(another_edl)
+  class List
+    attr_accessor :events
+    def initialize(events = [])
+      @events = events.dup
+    end
+    
+    # Return the same EDL with all dissolves stripped and replaced by the clips under them
+    def without_dissolves
+      # Find dissolves
+      cpy = []
+      accum_offset = 0
+      
+      @events.each_with_index do | e, i |
+        # A dissolve always FOLLOWS the incoming clip
+        if @events[i+1].is_a?(Transition)
+          dissolve = @events[i+1]
+          len = dissolve.duration.to_i
+          
+          # Shift all events to the right
+          accum_offset += len
+          
+          # The dissolve contains the OUTGOING clip, we are the INCOMING. Extend the
+          # incoming clip by the length of the dissolve, that's the whole mission actually
+          incoming = e.copy_properties_to(Clip.new)
+          incoming.src_end_tc += len
+          incoming.rec_end_tc += len
+          
+          outgoing = dissolve.copy_properties_to(Clip.new)
+          
+          # Add the A suffix to the ex-dissolve
+          outgoing.num += 'A'
+          
+          # Take care to join the two if they overlap - TODO
+          cpy << incoming
+          cpy << outgoing
+        elsif e.is_a?(Transition)
+          # Skip, already handled!
+        else
+          cpy << e.dup
+        end
+      end
+      # Do not renumber events
+      # (0...cpy.length).map{|e| cpy[e].num = "%03d" % e }
+      self.class.new(cpy)
+    end
+    
+    def renumbered
+      renumed = @events.dup
+      pad = renumed.length.to_s.length
+      pad = 3 if pad < 3
+      
+      (0...renumed.length).map{|e| renumed[e].num = "%0#{pad}d" % (e+1) }
+      x = self.class.new(renumed)
+      puts x.inspect
+      x
+    end
+    
+    # Return the same EDL with all timewarps expanded to native length. Clip length
+    # changes have rippling effect on footage that comes after the timewarped clip
+    # (so this is best used in concert with the original EDL where record TC is pristine)
+    def without_timewarps
+      raise "Implement me"
+    end
+    
+    # Return the same EDL without AX, BL and other GEN events (like slug, text and solids).
+    # Usually used in concert with "without_dissolves"
+    def without_generators
+      gen_reels = %w(AX BL GEN)
+      self.class.new(@events.reject{|e| gen_reels.include?(e.reel) })
+    end
+    
+    def spliced
+      spliced_edl = @events.inject([]) do | spliced, cur  |
+        latest = spliced[-1]
+        # Append to latest if splicable
+        if latest && (latest.reel == cur.reel) && (cur.src_start_tc == (latest.src_end_tc + 1))
+          latest.src_end_tc = cur.src_end_tc
+          latest.rec_end_tc = cur.rec_end_tc
+        else
+          spliced << cur.dup
+        end
+        spliced
+      end
+      self.class.new(spliced_edl)
+    end
+  end
   
   class Event
     attr_accessor :num, 
@@ -13,69 +104,125 @@ module EDL
       :comments,
       :original_line
     def to_s
-      %w( num reel track src_start_tc src_end_tc rec_start_tc rec_end_tc).join("  ")
+      %w( num reel track src_start_tc src_end_tc rec_start_tc rec_end_tc).map{|a| self.send(a).to_s}.join("  ")
+    end
+    
+    def inspect
+      to_s
+    end
+    
+    def copy_properties_to(evt)
+      %w( num reel track src_start_tc src_end_tc rec_start_tc rec_end_tc).each do | k |
+        evt.send("#{k}=", send(k)) if evt.respond_to?(k)
+      end
+      evt
     end
   end
   
   class Clip < Event
-    attr_accessor :clip_name
+    attr_accessor :clip_name, :timewarp_speed
   end
   
   class VideoClip < Clip; end
-  class AudioClip < Clip; end
   
   class Transition < Event
-    attr_accessor :effect, :duration
+    attr_accessor :effect, :duration, :timewarp_speed, :clip_name
   end
   
   class Black < Clip; end
-  class Generator < Event; end
+  class Generator < Clip; end
   
-  COMMENT_MATCHERS = {
-    /FROM CLIP NAME:(\w+)/ => lambda { | evt, match | evt.clip_name = match.flatten.pop.strip },
-    /EFFECT NAME:(\.+)/ => lambda { | evt, match | evt.effect = match.flatten.pop.strip }
-  }
-  
-  class Parser
-    TC = /(\d{1,2}):(\d{1,2}):(\d{1,2}):(\d{1,2})/
-
-    # 021  009      V     C        00:39:04:21 00:39:05:09 01:00:26:17 01:00:27:05
-    EVENT_PAT = /(\d+)(\s+)(\w+)(\s+)(\w+)(\s+)(\w+)(\s+)((\w+\s+)?)#{TC} #{TC} #{TC} #{TC}/
-    COMMENT_PAT = /^\* /
-    
-    def event_line?(line)
-      !!(line.strip =~ EVENT_PAT)
-    end
-
-    def comment_line?(line)
-      !!(line.strip =~ COMMENT_PAT)
-    end
-    
-    def parse(io)
-      until io.eof?
-        current_line = io.gets
-        
-        if event_line?(current_line)
-          push_event if @event
-          @event = event_from_line(current_line)
-        elsif comment_line?(current_line)
-          COMMENT_MATCHERS.each_pair do | pattern, transformer |
-            scan_result = current_line.scan(pattern)
-            transformer.call(@event, scan_result) if scan_result
-          end
-        end
+  class Matcher
+    class ApplyError < RuntimeError
+      def initialize(msg, line)
+        super("%s - offending line was '%s'" % [msg, line])
       end
     end
     
-    def push_event
-      @events ||= []
-      @events << @event
+    def initialize(with_regexp)
+      @regexp = with_regexp
     end
     
-    def event_from_line(line)
-      line.strip!
+    def signals_new_event?
+      false
+    end
+    
+    def matches?(line)
+      line =~ @regexp
+    end
+    
+    def apply(stack, current_event, line)
+      STDERR.puts "Skipping #{line}"
+    end
+  end
+  
+  class NameMatcher < Matcher
+    def initialize
+      super(/\* FROM CLIP NAME:(\s+)(.+)/)
+    end
+    
+    def apply(stack, current_event, line)
+      current_event.clip_name = line.scan(@regexp).flatten.pop.strip
+    end
+  end
+  
+  class EffectMatcher < Matcher
+    def initialize
+      super(/\* EFFECT NAME:(\s+)(.+)/)
+    end
+    
+    def apply(stack, current_event, line)
+      current_event.effect = line.scan(@regexp).flatten.pop.strip
+    end
+  end
+  
+  class TimewarpMatcher < Matcher
+    def initialize
+      @regexp = /M2(\s+)(\w+)(\s+)(\d+\.\d+)(\s+)(\d{1,2}):(\d{1,2}):(\d{1,2}):(\d{1,2})/
+    end
+    
+    def apply(stack, current_event, line)
+      all_evts = stack + [current_event]
+      matches = line.scan(@regexp).flatten.map{|e| e.strip}.reject{|e| e.nil? || e.empty?}
       
-      matches = line.scan(EVENT_PAT).shift
+      from_reel = matches.shift
+      fps = matches.shift
+      
+      begin
+        tw_start_source_tc = Parser.timecode_from_line_elements(matches)
+      rescue Timecode::Error => e
+        raise ApplyError, "Invalid TC in timewarp (#{e})", line
+      end
+      
+      evt_with_tw = all_evts.reverse.find{|e| e.src_start_tc == tw_start_source_tc && e.reel == from_reel }
+
+      unless evt_with_tw
+        raise ApplyError, "Cannot find event marked by timewarp", line
+      else
+        evt_with_tw.timewarp_speed = (25.0/100.0) * fps.to_f
+      end
+      
+    end
+  end
+  
+  # Drop frame goodbye
+  TC = /(\d{1,2}):(\d{1,2}):(\d{1,2}):(\d{1,2})/
+  
+  class EventMatcher < Matcher
+    def signals_new_event?
+      true
+    end
+
+    # 021  009      V     C        00:39:04:21 00:39:05:09 01:00:26:17 01:00:27:05
+    EVENT_PAT = /(\d+)(\s+)(\w+)(\s+)(\w+)(\s+)(\w+)(\s+)((\w+\s+)?)#{TC} #{TC} #{TC} #{TC}/
+    
+    def initialize
+      super(EVENT_PAT)
+    end
+    
+    def apply(stack, cur_evt, line)
+      
+      matches = line.scan(@regexp).shift
       props = {:original_line => line}
       
       # FIrst one is the event number
@@ -103,25 +250,70 @@ module EDL
       matches.shift
       
       # Then the timecodes
-      props[:src_start_tc] = timecode_from_line_elements(matches)
-      props[:src_end_tc] = timecode_from_line_elements(matches)
-      props[:rec_start_tc] = timecode_from_line_elements(matches)
-      props[:rec_end_tc] = timecode_from_line_elements(matches)
+      [:src_start_tc, :src_end_tc, :rec_start_tc, :rec_end_tc].each do | k |
+        begin
+          props[k] = EDL::Parser.timecode_from_line_elements(matches)
+        rescue Timecode::Error
+          raise "Cannot parse timecode in #{line}"
+        end
+      end
       
       evt = case props.delete(:type)
         when 'C'
-          props[:track] == 'V' ? VideoClip.new : AudioClip.new
+          if props[:track] == 'V'
+            if props[:reel] == 'BL'
+              Black.new
+            elsif props[:reel] == 'AX'
+              Generator.new
+            else
+              VideoClip.new
+            end
+          else
+            AudioClip.new
+          end
         when 'D'
           Transition.new
         else
           Event.new
       end
-      puts props.inspect
       props.each_pair { | k, v | evt.send("#{k}=", v) }
       evt
     end
+  end
+  
+  class Parser
+    MATCHERS = [
+      EventMatcher.new,
+      EffectMatcher.new,
+      NameMatcher.new,
+      TimewarpMatcher.new,
+    ]
     
-    def timecode_from_line_elements(elements)
+    def parse(io)
+      @stack, cur_evt = [], nil
+      until io.eof?
+        current_line = io.gets.strip
+        MATCHERS.each do | matcher |
+          next unless matcher.matches?(current_line)
+          if matcher.signals_new_event?
+            @stack.push(cur_evt) if cur_evt # Start afresh
+            cur_evt = matcher.apply(@stack, cur_evt, current_line)
+          else
+            begin
+              matcher.apply(@stack, cur_evt, current_line)
+            rescue Matcher::ApplyError => e
+              STDERR.puts "Cannot parse #{current_line} - #{e}"
+            end
+          end
+        end
+      end
+      
+      # The last remaining event
+      @stack.push(cur_evt) if cur_evt
+      return List.new(@stack)
+    end
+    
+    def self.timecode_from_line_elements(elements)
       args = (0..3).map{|_| elements.shift.to_i} + [@fps || 25]
       Timecode.at(*args)
     end
--- test/test_edl.rb
+++ test/test_edl.rb
@@ -19,8 +19,58 @@ class TestParser < Test::Unit::TestCase
     assert_nothing_raised { EDL::Parser.new }
   end
   
-  TRAILER_EDL = File.read(File.dirname(__FILE__) + '/samples/TRAILER_EDL.edl')
+  TRAILER_EDL = File.dirname(__FILE__) + '/samples/TRAILER_EDL.edl'
+  SIMPLE_DISSOLVE = File.dirname(__FILE__) + '/samples/SIMPLE_DISSOLVE.edl'
+  SPLICEME = File.dirname(__FILE__) + '/samples/SPLICEME.edl'
   
+  def test_timecode_from_elements
+    elems = ["08", "04", "24", "24"]
+    assert_nothing_raised { @tc = EDL::Parser.timecode_from_line_elements(elems) }
+    assert_kind_of Timecode, @tc
+    assert_equal "08:04:24:24", @tc.to_s
+    assert elems.empty?, "The elements should have been removed from the array"
+  end
+  
+  def test_dissolve
+    p = EDL::Parser.new
+    assert_nothing_raised{ @edl = p.parse File.open(SIMPLE_DISSOLVE) }
+    assert_kind_of EDL::List, @edl
+    assert_equal 2, @edl.events.length
+    
+    first = @edl.events[0]
+    assert_kind_of EDL::Clip, first
+    
+    second = @edl.events[1]
+    assert_kind_of EDL::Transition, second
+    
+    no_trans = @edl.without_dissolves
+    
+    assert_equal 2, no_trans.events.length
+    assert_equal (Timecode.parse('01:00:00:00') + 43).to_s, no_trans.events[0].rec_end_tc.to_s, 
+      "The incoming clip should have been extended by the length of the dissolve"
+      
+    assert_equal Timecode.parse('01:00:00:00').to_s, no_trans.events[1].rec_start_tc.to_s
+      "The outgoing clip should have been left in place"
+  end
+  
+  def test_spliced
+    p = EDL::Parser.new
+    assert_nothing_raised{ @edl = p.parse(File.open(SPLICEME)) }
+    assert_equal 3, @edl.events.length
+    
+    spliced = @edl.spliced
+    assert_equal 1, spliced.events.length, "Should have been spliced to one event"
+  end
+end
+
+class TimewarpMatcherTest < Test::Unit::TestCase
+  def test_needs_to_be_written
+    flunk
+  end
+end
+
+class EventMatcherTest < Test::Unit::TestCase
+
   EVT_PATTERNS = [
     '020  008C     V     C        08:04:24:24 08:04:25:19 01:00:25:22 01:00:26:17', 
     '021  009      V     C        00:39:04:21 00:39:05:09 01:00:26:17 01:00:27:05', 
@@ -30,40 +80,11 @@ class TestParser < Test::Unit::TestCase
     '025        BL V     C        00:00:00:00 00:00:00:00 01:00:29:19 01:00:29:19', 
     '025  GEN      V     D    025 00:00:55:10 00:00:58:11 01:00:29:19 01:00:32:20',
   ]
-  
-  CMT_PATTERNS = [
-    "* FROM CLIP NAME:  SLUG",
-    "* FROM CLIP IS A STILL",
-    "* PROBLEM WITH EDIT: CLIP HAD NO TIMECODE TRACK"
-  ]
-  
-  def test_main_patterns
-    p = EDL::Parser.new
-    EVT_PATTERNS.each do | evt_line |
-      assert p.event_line?(evt_line), "#{evt_line} should be recognized as event line"
-    end
-  end
-  
-  def test_cmt_patterns
-    p = EDL::Parser.new
-    CMT_PATTERNS.each do | cmt_line |
-      assert p.comment_line?(cmt_line), "#{cmt_line} should be recognized as event line"
-    end
-  end
-  
-  def test_timecode_from_elements
-    p = EDL::Parser.new
-    elems = ["08", "04", "24", "24"]
-    assert_nothing_raised { @tc = p.timecode_from_line_elements(elems) }
-    assert_kind_of Timecode, @tc
-    assert_equal "08:04:24:24", @tc.to_s
-    assert elems.empty?, "The elements should have been removed from the array"
-  end
-  
+
   def test_clip_generation_from_line
-    p = EDL::Parser.new
+    m = EDL::EventMatcher.new
     
-    clip = p.event_from_line(
+    clip = m.apply(nil, nil,
       '020  008C     V     C        08:04:24:24 08:04:25:19 01:00:25:22 01:00:26:17'
     )
     
@@ -79,11 +100,9 @@ class TestParser < Test::Unit::TestCase
     assert_equal '020  008C     V     C        08:04:24:24 08:04:25:19 01:00:25:22 01:00:26:17', clip.original_line
   end
   
-  
   def test_dissolve_generation_from_line
-    p = EDL::Parser.new
-    
-    dissolve = p.event_from_line(
+    m = EDL::EventMatcher.new
+    dissolve = m.apply(nil, nil,
       '025  GEN      V     D    025 00:00:55:10 00:00:58:11 01:00:29:19 01:00:32:20'
     )
     assert_not_nil dissolve
@@ -93,6 +112,52 @@ class TestParser < Test::Unit::TestCase
     assert_equal 'V', dissolve.track
     assert_equal '025', dissolve.duration
     assert_equal '025  GEN      V     D    025 00:00:55:10 00:00:58:11 01:00:29:19 01:00:32:20', dissolve.original_line
-    
+  end
+  
+  def test_black_generation_from_line
+    m = EDL::EventMatcher.new
+    black = m.apply(nil, nil,
+      '025        BL V     C        00:00:00:00 00:00:00:00 01:00:29:19 01:00:29:19' 
+    )
+    assert_not_nil black
+    assert_kind_of EDL::Black, black
+    assert_equal '025', black.num
+    assert_equal 'BL', black.reel
+    assert_equal 'V', black.track
+    assert_equal '025        BL V     C        00:00:00:00 00:00:00:00 01:00:29:19 01:00:29:19', black.original_line
+  end
+  
+  def test_matches_all_patterns
+    EVT_PATTERNS.each do | pat |
+      assert EDL::EventMatcher.new.matches?(pat), "EventMatcher should match #{pat}"
+    end
+  end
+end
+
+class ClipNameMatcherTest < Test::Unit::TestCase
+  def test_matches
+    line = "* FROM CLIP NAME:  TAPE_6-10.MOV"
+    assert EDL::NameMatcher.new.matches?(line)
+  end
+  
+  def test_apply
+    line = "* FROM CLIP NAME:  TAPE_6-10.MOV"
+    mok_evt = flexmock
+    mok_evt.should_receive(:clip_name=).with('TAPE_6-10.MOV').once
+    EDL::NameMatcher.new.apply([], mok_evt, line)
+  end
+end
+
+class EffectMatcherTest < Test::Unit::TestCase
+  def test_matches
+    line = "* EFFECT NAME: CROSS DISSOLVE"
+    assert EDL::EffectMatcher.new.matches?(line)
+  end
+  
+  def test_apply
+    line = "* EFFECT NAME: CROSS DISSOLVE"
+    mok_evt = flexmock
+    mok_evt.should_receive(:effect=).with('CROSS DISSOLVE').once
+    EDL::EffectMatcher.new.apply([], mok_evt, line)
   end
 end
