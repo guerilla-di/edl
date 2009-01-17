8e8272e8d7ead99ecf6627e2a849d2e0feb0bef7
Julik <me@julik.nl>
2009-01-18 00:02:06 +0100

    Extract parser into its own file

---
+++ lib/edl/parser.rb
@@ -0,0 +1,48 @@
+module EDL
+  
+  # Used to parse an EDL
+  class Parser
+  
+    attr_reader :fps
+  
+    # Initialize an EDL parser. Pass the FPS to it, as the usual EDL does not contain any kind of reference 
+    # to it's framerate
+    def initialize(with_fps = DEFAULT_FPS)
+      @fps = with_fps
+    end
+  
+    def get_matchers #:nodoc:
+      [ EventMatcher.new(@fps), EffectMatcher.new, NameMatcher.new, TimewarpMatcher.new(@fps), CommentMatcher.new ]
+    end
+  
+    # Parse a passed File or IO object line by line, or the whole string
+    def parse(io)
+      return parse(StringIO.new(io.to_s)) unless io.respond_to?(:eof?)
+    
+      stack, matchers = List.new, get_matchers
+    
+      at_line = 0
+      until io.eof?
+        at_line += 1
+
+        current_line = io.gets.strip
+        m = matchers.find{|m| m.matches?(current_line) }
+        next unless m
+      
+        begin
+          m.apply(stack, current_line)
+          stack[-1].line_number = at_line if m.is_a?(EventMatcher)
+        rescue Matcher::ApplyError => e
+          STDERR.puts "Cannot parse #{current_line} - #{e}"
+        end
+      end
+      stack
+    end
+  
+    # Init a Timecode object from the passed elements with the passed framerate
+    def self.timecode_from_line_elements(elements, fps) #:nodoc:
+      args = (0..3).map{|_| elements.shift.to_i} + [fps.to_f]
+      Timecode.at(*args)
+    end
+  end
+end
